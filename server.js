3ee1f2245c15f608a94c34b1a2c35a407a9b2206
Anay Mittal <anayttal@gmail.com>
2022-05-15 20:15:49 -0400

    Camp commit

--- server.js
+++ server.js
@@ -5,18 +5,36 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+var players = [];
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+    //Add existing players
+    io.to(socket.id).emit('addExistingPlayers', players);
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('addPlayer', (data) => {
+        data.push(socket.id)
+        players.push(data);
+
+        socket.broadcast.emit('addPlayerToOthers', data);
+    });
+
+    //User leaves
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('userLeft', socket.id);
+    });
 });
 
+
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
